ebf429807cc53215b09b2641e09d51b7c71cd6c6
alikiminza <ali.kiminza@student.moringaschool.com>
2023-06-22 09:16:17 +0300

    done tests

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, match) {
+  return drivers.filter(driver => driver.toLowerCase() === match.toLowerCase());
+}
+
+function fuzzyMatch(drivers, trial) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(trial.toLowerCase()));
+  }
+
+  function matchName(drivers, final) {
+    return drivers.filter(driver => driver.name.toLowerCase() === final.toLowerCase());
+  }
